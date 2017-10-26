c43269cfa8a6b9698b2a7c5b750314095d269e84
Michał Kaciuba <mkaciuba@MacBook-Pro-Micha.local>
2017-10-26 18:05:14 +0200

    modify install script

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-chmod 660 userpanel/* -R
+chmod 644 userpanel/* -R
 cp -R userpanel/* ../../userpanel/
+rm -rf ../../userpanel/templates_c/*
 
 openssl genrsa -out lms.pem 2048
 openssl rsa -in lms.pem -pubout > lms.pub
+
+cd ../../
+composer install && composer update
+cd userpanel/style/bclean
+composer install && composer update
