6758dc405aa2839ec4261109423be19f8b91c5a5
pablos-billtech <69465966+pablos-billtech@users.noreply.github.com>
2021-03-16 14:11:33 +0100

    Poprawka sprawdzenia billtech.api_url (#59)

--- lib/BillTechApiClientFactory.php
+++ lib/BillTechApiClientFactory.php
@@ -6,12 +6,20 @@ class BillTechApiClientFactory {
 		$api_url = ConfigHelper::getConfig('billtech.api_url');
 		return new Client([
 			'base_uri' =>
-				str_ends_with($api_url, '/') ?
+				self::endsWith($api_url, '/') ?
 					substr_replace($api_url, '', -1, 1) : $api_url,
 			'auth' => [
 				ConfigHelper::getConfig('billtech.api_key'),
 				ConfigHelper::getConfig('billtech.api_secret')
 			]
 		]);
+	}
+
+	private static function endsWith($haystack, $needle) {
+		$length = strlen($needle);
+		if(!$length) {
+			return false;
+		}
+		return substr($haystack, -$length) === $needle;
 	}
 }
