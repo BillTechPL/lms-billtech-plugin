7ad0100315a1eab881fc6ef3f7314c774aa160b4
Sebastian Miecielica <smiecielica@billtech.pl>
2022-01-12 16:46:31 +0100

    Add migration db files

---
+++ lib/upgradedb/mysql.2022011200.php
@@ -0,0 +1,18 @@
+<?php
+$this->Execute("
+ALTER TABLE billtech_payment_links
+DROP FOREIGN KEY billtech_payment_links_src_cash_id_fkey,
+                ADD CONSTRAINT billtech_payment_links_src_cash_id_fkey
+FOREIGN KEY (src_cash_id) REFERENCES cash ON
+DELETE
+SET NULL
+");
+
+$this->Execute("
+ALTER TABLE billtech_payment_links
+DROP FOREIGN KEY billtech_payment_links_src_document_id_fkey,
+                ADD CONSTRAINT billtech_payment_links_src_document_id_fkey
+FOREIGN KEY (src_document_id) REFERENCES documents ON
+DELETE
+SET NULL
+");
