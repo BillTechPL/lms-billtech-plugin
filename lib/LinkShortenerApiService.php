5260efca9635fbe4615c8bbbb0cd7f8c23c9f976
Sebastian Miecielica <smiecielica@billtech.pl>
2022-08-16 15:52:07 +0200

    Formatting again

--- lib/LinkShortenerApiService.php
+++ lib/LinkShortenerApiService.php
@@ -10,17 +10,17 @@ use GuzzleHttp\Exception\ClientException;
 
 class LinkShortenerApiService
 {
-	private $client;
+    private $client;
 
-	public function __construct()
-	{
-		$this->client = new Client([
-			'base_uri' => 'https://zapl.ac',
-		]);
-	}
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://zapl.ac',
+        ]);
+    }
 
-	public function addParameters($url, $params = array())
-	{
+    public function addParameters($url, $params = array())
+    {
         try {
             $response = $this->client->post('/encode', [
                 'query' => $params,
