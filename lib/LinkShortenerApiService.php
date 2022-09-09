870c29bc0654e1c8afeab10d9df9c0bb4c5d6ae7
smiecielica <91327379+smiecielica@users.noreply.github.com>
2022-09-09 17:46:38 +0200

    Bugfix/catch guzzle post errors (#97)
    Add retrying for url encoding request

--- lib/LinkShortenerApiService.php
+++ lib/LinkShortenerApiService.php
@@ -21,6 +21,28 @@ class LinkShortenerApiService
 
 	public function addParameters($url, $params = array())
 	{
+		$retryLimit = 3;
+		$retryCount = 1;
+
+		while($retryCount <= $retryLimit) {
+			try {
+				return $this->postEncodeUrl($url, $params);
+			} catch (Exception $e) {
+				$retryCount++;
+				sleep(1);
+				if($retryCount === $retryLimit) {
+					$error = $e->getResponse();
+					echo "Unable to add parameters to the link. Server response: ".$error;
+				}
+				continue;
+			}
+		}
+
+		return "";
+	}
+
+	private function postEncodeUrl($url, $params)
+	{
 		$response = $this->client->post('/encode', [
 			'query' => $params,
 			'json' => [
